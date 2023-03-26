3dedf9eed40984cbecc858aeb4b420fba6e01aa6
Stanley Skarshaug <stanley.skarshaug@gmail.com>
2023-03-26 21:15:14 +0200

    remove unused placehodler top level tab

--- gui.py
+++ gui.py
@@ -16,17 +16,10 @@ mainTabControl = ttk.Notebook(window)
 elementraySchoolTab = ttk.Frame(mainTabControl)
 mainTabControl.add(elementraySchoolTab, text ='Elementary School')
 
-randomTab = ttk.Frame(mainTabControl)
-mainTabControl.add(randomTab, text ='Various Problems')
-
 mainTabControl.pack(expand = 1, fill ="both")
 
 gui.elementary.Tab(elementraySchoolTab)
 
-
-## Various Problems Tab
-tab_2_content = ttk.Frame(randomTab)
-tab_2_content.pack(expand=1, fill="both")
 
 # Credits
 bottomFrame = ttk.Frame(window)
